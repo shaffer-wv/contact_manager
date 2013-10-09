a9cd09d2b9297fff3dce385162d2888cb8a04366
shaffer-wv <shaffer-wv@outlook.com>
2013-10-08 20:23:28 -0400

    added more phone number tests

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -2,4 +2,5 @@ class PhoneNumber < ActiveRecord::Base
   attr_accessible :number, :person_id
 
   validates :number, presence: true
+  validates :person_id, presence: true
 end
--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe PhoneNumber do
   let(:phone_number) do
-  	PhoneNumber.new(number: '2023139900')
+  	PhoneNumber.new(number: '2023139900', person_id: 1)
   end
 
   it 'is valid' do
@@ -12,5 +12,10 @@ describe PhoneNumber do
   it 'is invalid without a number' do
   	phone_number.number = nil
   	expect(phone_number).not_to be_valid
+  end
+
+  it 'must ahve a reference to a person' do
+  	phone_number.person_id = nil
+  	expect(phone_number).to_not be_valid
   end
 end
