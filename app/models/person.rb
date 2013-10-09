485ad5ec629a0efc928428ec7ed81d6652f504bd
shaffer-wv <shaffer-wv@outlook.com>
2013-10-08 20:08:38 -0400

    Generate phone number and associate with person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,8 @@
 class Person < ActiveRecord::Base
   attr_accessible :first_name, :last_name
+
+  has_many :phone_numbers
+  
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
