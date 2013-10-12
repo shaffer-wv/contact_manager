e161b977c1d248c3fa22564182bd4e312c463ff7
shaffer-wv <shaffer-wv@outlook.com>
2013-10-11 21:40:00 -0400

    Added validation to Email Addresses

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe EmailAddress do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  let(:email_address) { EmailAddress.new(address: 'nobody@yahoo.com', person_id: 4) }
+
+  it 'is valid' do
+  	expect(email_address).to be_valid
+  end
+
+  it 'is invalid without a first_name' do
+  	email_address.address = nil
+  	expect(email_address).not_to be_valid
+  end
 end
