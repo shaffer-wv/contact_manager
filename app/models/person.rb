14acf263d94af4df3b2b442fced698b4f02498bf
shaffer-wv <shaffer-wv@outlook.com>
2013-10-11 21:28:48 -0400

    added relationship between email addresses and people

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,6 +2,7 @@ class Person < ActiveRecord::Base
   attr_accessible :first_name, :last_name
 
   has_many :phone_numbers
+  has_many :email_addresses
   
   validates :first_name, presence: true
   validates :last_name, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131009000621) do
+ActiveRecord::Schema.define(:version => 20131012012439) do
+
+  create_table "email_addresses", :force => true do |t|
+    t.string   "address"
+    t.integer  "person_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "people", :force => true do |t|
     t.string   "first_name"
